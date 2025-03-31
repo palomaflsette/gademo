36634437d936ea4e2880828326a47f7839a892c6
Paloma Sette <palomafl.sette@gmail.com>
2025-03-31 00:07:48 -0300

    :wrench:  configuração automática do endpoint da API conforme o ambiente.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-window.API_ENDPOINT = 'https://gademo-api.onrender.com';
-//window.API_ENDPOINT = 'http://127.0.0.1:8000';
-//window.API_ENDPOINT = 'https://servicosmaxwell.vrac.puc-rio.br';
-//window.API_ENDPOINT = 'http://127.0.0.1
+if (window.location.hostname.includes('vrac.puc-rio.br')) {
+    window.API_ENDPOINT = 'https://servicosmaxwell.vrac.puc-rio.br';
+} else if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') {
+    window.API_ENDPOINT = 'http://127.0.0.1:8000';
+} else {
+    window.API_ENDPOINT = 'https://gademo-api.onrender.com';
+}
