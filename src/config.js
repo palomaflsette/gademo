98eab906051b4e6a0ed4c215ffccdea23b91fd33
Paloma Sette <palomafl.sette@gmail.com>
2025-04-01 12:56:58 -0300

    changing config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-if (window.location.hostname.includes('vrac.puc-rio.br')) {
-    window.API_ENDPOINT = 'https://servicosmaxwell.vrac.puc-rio.br';
+if (window.location.hostname.includes('onrender.com')) {
+    window.API_ENDPOINT = 'https://gademo-api.onrender.com';
 } else if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') {
     window.API_ENDPOINT = 'http://127.0.0.1:8000';
 } else {
-    window.API_ENDPOINT = 'https://gademo-api.onrender.com';
+    window.API_ENDPOINT = 'https://servicosmaxwell.vrac.puc-rio.br';
 }
