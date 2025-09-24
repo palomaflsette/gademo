c9f059ab85fdbc9860e66dc544fc2baef38df0a1
Paloma Sette <palomafl.sette@gmail.com>
2025-09-24 11:43:11 -0300

    alterando a config para funcionamento da api no pages

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,15 @@
 if (window.location.hostname.includes('onrender.com')) {
     window.API_ENDPOINT = 'https://gademo-api.onrender.com';
-} else if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') {
+} else if (
+    window.location.hostname === '127.0.0.1' ||
+    window.location.hostname === 'localhost'
+) {
     window.API_ENDPOINT = 'http://127.0.0.1:8000';
+} else if (
+    window.location.hostname.includes('github.io') ||
+    window.location.hostname.includes('palomasette.com')
+) {
+    window.API_ENDPOINT = 'https://gademo-api.onrender.com';
 } else {
     window.API_ENDPOINT = 'https://servicosmaxwell.vrac.puc-rio.br';
 }
