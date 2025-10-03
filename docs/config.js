e8429e8e9ade1a3bf7c236bdfb9ea7bbf48c1061
palomaflsette <palomafl.sette@gmail.com>
2025-10-03 03:56:36 -0300

    retornando ao onrender para teste

--- docs/config.js
+++ docs/config.js
@@ -9,7 +9,7 @@ if (window.location.hostname.includes('onrender.com')) {
     window.location.hostname.includes('github.io') ||
     window.location.hostname.includes('palomasette.com')
 ) {
-    window.API_ENDPOINT = 'https://gademo-api-80847e4a6e18.herokuapp.com';
+    window.API_ENDPOINT = 'https://gademo-api.onrender.com';
 } else {
     window.API_ENDPOINT = 'https://servicosmaxwell.vrac.puc-rio.br';
 }
